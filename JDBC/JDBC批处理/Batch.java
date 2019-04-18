2c0961f1a1e9a83debe05933d6ea8f4b8eeeb794
chenkaigao <49379693+chenkaigao@users.noreply.github.com>
2019-04-18 12:45:30 +0800

    1

---
+++ JDBC/JDBC批处理/Batch.java
@@ -0,0 +1,35 @@
+package com.kico.test;
+
+import com.kico.jdbcutil.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Batch {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = JDBCUtils.getCoonection();
+        PreparedStatement ps = null;
+        try {
+            String sql = "insert into user1 (name,password) values(?,?)";
+            ps = conn.prepareStatement(sql);
+            long begin = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                ps.setString(1, "赵111");
+                ps.setString(2, "111");
+                //添加批处理
+                ps.addBatch();
+            }
+//            执行批处理
+            ps.executeBatch();
+            long end = System.currentTimeMillis();
+            long time = end - begin;
+            System.out.println(time);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(conn, ps, null);
+        }
+
+    }
+}
