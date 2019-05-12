b05a93a82a8761b37f09f348dd04cec7e8698381
chenkaigao <49379693+chenkaigao@users.noreply.github.com>
2019-05-13 00:13:58 +0800

    1

---
+++ Tomcat/通用Servlet/TestServlet.java
@@ -0,0 +1,39 @@
+package com.myxq;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends BaseServlet {
+	
+	public String add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("添加");
+		return "add.jsp";
+	}
+
+	public String del(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("删除");
+		return "del.jsp";
+	}
+
+	public String  update(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("更新");
+		return "update.jsp";
+	}
+	
+	public String  find(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("查找");
+		return "find.jsp";
+	}
+}
